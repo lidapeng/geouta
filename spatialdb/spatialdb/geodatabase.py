c001d5928d0e7cb7f10b78d3bbb2b2c935a393e0
Dapeng Li <edward.1985@163.com>
2016-08-20 09:40:00 -0400

    add files

---
+++ spatialdb/spatialdb/geodatabase.py
@@ -0,0 +1,16 @@
+'''
+Created on Aug 20, 2016
+
+@author: Dapeng Li
+'''
+
+class PostgreSQLDB(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
